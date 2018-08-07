ca7af157076f31fdb6e567b42b3ee221d4124ede
boyang1020 <boyang1020@gmail.com>
2018-08-06 21:10:08 -0700

    register page

---
+++ app/screens/RegisterScreen.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { ScrollView, StyleSheet } from 'react-native';
+import RegisterPage from '../containers/RegisterContainer/RegisterPage';
+export default class RegisterScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Register',
+  };
+
+  render() {
+
+    return (
+      <RegisterPage />
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 15,
+    backgroundColor: '#fff',
+  },
+});
+
