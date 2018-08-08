02258424ef79a0a1e635a9fff64167a34381a4af
ellamarie005 <ellatolentino05@gmail.com>
2018-08-07 23:18:47 -0700

    merging

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import { createSwitchNavigator } from 'react-navigation';
-import Login from '../containers/LoginContainer/LoginPage';
 import MainTabNavigator from './MainTabNavigator';
 
 export default createSwitchNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-  Login: Login,
   Main: MainTabNavigator,
 });
