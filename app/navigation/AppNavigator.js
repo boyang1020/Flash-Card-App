b8c52e49b623e31479ecd0569c8555a9a2666084
ellamarie005 <ellatolentino05@gmail.com>
2018-08-08 18:22:41 -0700

    errors have been removed

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import { createSwitchNavigator } from 'react-navigation';
-<<<<<<< HEAD
-=======
-import Login from '../containers/LoginContainer/LoginPage';
->>>>>>> 3c0ee90ef08b6ac4b1e1cb011a1bed52b0919502
 import MainTabNavigator from './MainTabNavigator';
 
 export default createSwitchNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-  Login: Login,
   Main: MainTabNavigator,
 });
