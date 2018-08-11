4d492eaed7dd4aff79241eed57e1d996c77fb5fb
JP Medallon <jpmedallon@gmail.com>
2018-08-11 11:44:47 -0700

    Added new buttons and navigation to homepage (#21)
    * edited homepage to have redirects to login and register
    
    * edited homepage to have redirects to login and register
    
    * edited homepage to have redirects to login and register
    
    * edited homepage to have redirects to login and register
    
    * added styling to match register and login pages
    
    * completed homepage rehaul, with new fonts and branding

--- app/components/StyledText.js
+++ app/components/StyledText.js
@@ -3,6 +3,6 @@ import { Text } from 'react-native';
 
 export class MonoText extends React.Component {
   render() {
-    return <Text {...this.props} style={[this.props.style, { fontFamily: 'Menlo-Regular' }]} />;
+    return <Text {...this.props} style={[this.props.style, { fontFamily: 'Menlo-Regular', fontFamily:'AvenirNext-Regular' }]} />;
   }
 }
