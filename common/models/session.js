d5ab41faa8b8cc56a86c33c0f56010d38c754d75
EllaT <ellatolentino05@gmail.com>
2018-08-28 20:45:42 -0700

    New Deck Remote Method (#33)
    * added missing lines at the end of the files and updated README
    
    * created remote method with formula 80% cards with score 0 and 20% with score 1 or 2

--- common/models/session.js
+++ common/models/session.js
@@ -1,5 +1,29 @@
 'use strict';
 
-module.exports = function(Session) {
+module.exports = function (Session) {
+  Session.newDeck = function (id, newDeck) {
+    var cards;
+    setTimeout(() => {
+      Session.app.models.Card
+        .find({ where: { score: 0 }, limit: 8 })
+        .then(res => {
+          var scoreZero = res;
+          Session.app.models.Card
+            .find({ where: { score: 1 || 2 }, limit: 2 })
+            .then(res => {
+              return newDeck(null, cards = scoreZero.concat(res));
+            })
+        })
+    }, 5000)
+  }
 
+  Session.remoteMethod('newDeck',
+    {
+      http: { path: '/:id/newDeck', verb: 'get' },
+      description: 'Returns cards for the new session',
+      accepts: [{ arg: 'id', type: 'string' }],
+      returns: {
+        arg: 'newDeck', type: 'object'
+      }
+    });
 };
